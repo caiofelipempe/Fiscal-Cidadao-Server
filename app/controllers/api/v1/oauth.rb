79478de7b3d48f4c90445afdd0f31dd825799446
caiofelipedemourapeixoto <caiofelipe.mpe@gmail.com>
2015-09-10 15:33:17 -0300

    .

--- app/controllers/api/v1/oauth.rb
+++ app/controllers/api/v1/oauth.rb
@@ -15,7 +15,7 @@ module API
 
       end
 
-      resource :token do
+      resource :oauth do
 
         desc "Register of a new user"
         params do
@@ -23,8 +23,18 @@ module API
           requires :login, type: String, desc: "Email address or login"
           requires :password, type: String, desc: "Password"
         end
-        post do
-          
+        post :token do
+          if params[:login].include?("@")
+            user = User.find_by_email(params[:login].downcase)
+          else
+            user = User.find_by_login(params[:login].downcase)
+          end
+
+          if user && user.valid_password?(params[:password])
+            # Create a token to user.
+          else
+            error!('Unauthorized.', 401)
+          end
         end
 
       end
