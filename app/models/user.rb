c89149cce14c1b3af4a5d4d307749206f5b9eaae
caiofelipedemourapeixoto <caiofelipe.mpe@gmail.com>
2015-09-16 15:25:40 -0300

    Mudada validação da imagem do usuário.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }, :dependent => :delete
+  has_attached_file :image, styles: { small: "64x64#", med: "100x100#", large: "200x200#" }, :dependent => :delete
 
   validates_attachment :image, :content_type => { :content_type => 'image/jpeg' }, :size => { :in => 0..1000.kilobytes }
   validates_format_of :login, without: /@/
