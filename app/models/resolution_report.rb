aaf63ddc6d83d439a10c256fcd43c53f83bd90ac
caiofelipedemourapeixoto <caiofelipe.mpe@gmail.com>
2015-09-23 14:31:15 -0300

    Mudado tamanho maximo da imagem do problema para 1000 kb.

--- app/models/resolution_report.rb
+++ app/models/resolution_report.rb
@@ -5,5 +5,5 @@ class ResolutionReport < ActiveRecord::Base
   validates :description, presence: true
 
   has_attached_file :image, styles: { small: "64x64#", med: "100x100#", large: "200x200#" }, :dependent => :delete
-  validates_attachment :image, :content_type => { :content_type => 'image/jpeg' }, :size => { :in => 0..5000.kilobytes }
+  validates_attachment :image, :content_type => { :content_type => 'image/jpeg' }, :size => { :in => 0..1000.kilobytes }
 end
