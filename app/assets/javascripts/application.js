1ad928e7b34c5ee65e67239a6740f1c01ecb3b1b
Jeovania <jeovaniaabreu@gmail.com>
2015-12-03 13:51:21 -0300

    layout teste incompleto

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,8 @@
 //= require jquery
 //= require jquery_ujs
-//= require angular/angular.min
-//= require angular-animate/angular-animate.min
-//= require angular-aria/angular-aria.min
-//= require angular-resource/angular-resource.min
-//= require angular-ui-router/release/angular-ui-router.min
-//= require angular-material/angular-material.min
 
+//= require material-design-lite/material
 //= require lightbox2/dist/js/lightbox.min
 
-//= require app
-//= require controllers/main.controller
 //= require turbolinks
 //= require_tree .
--- app/assets/javascripts/controllers/main.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('cidadaoApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
-    
-  }]);
