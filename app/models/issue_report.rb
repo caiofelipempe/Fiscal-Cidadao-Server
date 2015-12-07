1378d4634c2e8b1125f672320342e68a305a9c22
wedsonlima <wedson.sousa.lima@gmail.com>
2015-12-07 14:47:11 -0300

    rendering forms inside show

--- app/models/issue_report.rb
+++ app/models/issue_report.rb
@@ -1,6 +1,7 @@
 class IssueReport < ActiveRecord::Base
   belongs_to :user
   belongs_to :issue
+
   has_one :resolution_report, :dependent => :delete
 
   has_attached_file :image, styles: { thumb: "100x100>" }, :dependent => :delete
