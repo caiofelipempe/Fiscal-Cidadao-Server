291f8a82e6ff76902545967d449a75568b84936e
Jeovania <jeovaniaabreu@gmail.com>
2016-01-15 10:58:21 -0300

    link da index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  root to: "issue_reports#index"
+  root to: "issue_reports#index", :path => 'problemas'
 
   resources :issues, :path => 'categorias'
-  get "/resolvidos", to: 'issue_reports#resolvidos', as: :issue_reports_resolvidos
+  get "/problemas_resolvidos", to: 'issue_reports#resolvidos', as: :issue_reports_resolvidos
   resources :issue_reports, :except => [:new, :create], :path => 'problemas' do
-    # get 'resolution_report/new'
-    # post 'resolution_report/create'
     resources :resolution_report, :only => [:new, :create, :destroy]
   end
 
-  devise_for :users
+  devise_for :users, path: "/", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   use_doorkeeper
   mount API::Base => '/'
 end
